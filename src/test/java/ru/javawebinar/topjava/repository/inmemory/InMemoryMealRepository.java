1fce41b4ee05c8a58a283279e7e34ca0c121e91e
Igtaty Kruglov <iggythevegan@gmail.com>
2022-10-21 18:56:24 +0300

    Lesson 3 apply patch 3_11

--- src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,8 +1,10 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
